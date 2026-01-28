506e7a6525eec20d99ef8067a32eede5fb253984
Roman Pogoreltcev <romzes26@yandex.ru>
2026-01-28 23:36:37 +0700

    Postgresql + Docker

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,7 @@
 # user/urls.py
 from django.urls import path
 from . import views
-from user.views import LogoutView, LoginView
+
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
@@ -13,7 +13,7 @@ urlpatterns = [
     path('addresses/set-default/<int:pk>/', views.SetDefaultAddressView.as_view(), name='set-default-address'),
 
     # Вход
-    path('login/', LoginView.as_view(), name='login'),
+    # path('login/', LoginView.as_view(), name='login'),
     # Выход
     path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
